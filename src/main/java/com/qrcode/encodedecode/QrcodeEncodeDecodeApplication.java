4a62339d59d3967fc80a9c09b268db10ff4bed45
SohanBappy <sohanbappy@gmail.com>
2022-09-30 01:03:22 +0600

    Project initialized

---
+++ src/main/java/com/qrcode/encodedecode/QrcodeEncodeDecodeApplication.java
@@ -0,0 +1,13 @@
+package com.qrcode.encodedecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeEncodeDecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodeEncodeDecodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qrcode/encodedecode/QrcodeEncodeDecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qrcode.encodedecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeEncodeDecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
